8751e02a335146edbc9b5d05bfc713821d800542
manestep <manestep@gmail.com>
2020-07-26 22:46:29 +0400

    Add project files.

---
+++ DateTimeExtension/ExtensionClass.cs
@@ -0,0 +1,19 @@
+﻿
+
+using System;
+
+namespace DateTimeExtension
+{
+    public static class ExtensionClass
+    {
+        public static bool IsWeekPassed(this DateTime date)
+        {
+            return StartOfWeek(date, DayOfWeek.Monday) < StartOfWeek(DateTime.UtcNow, DayOfWeek.Monday);
+        }
+        public static DateTime StartOfWeek(this DateTime dt, DayOfWeek startOfWeek)
+        {
+            int diff = (7 + (dt.DayOfWeek - startOfWeek)) % 7;
+            return dt.AddDays(-1 * diff).Date;
+        }
+    }
+}
---
+++ DateTimeExtension/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeExtension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var date1 = new DateTime(2020, 7, 25); // false
+            var date2 = new DateTime(2020, 7, 20); // false
+            var date3 = new DateTime(2021, 7, 20); // false
+            var date4 = new DateTime(2020, 7, 19); // true
+            Console.WriteLine($"1:{date1.IsWeekPassed()}\n2:{date2.IsWeekPassed()}\n3:{date3.IsWeekPassed()}\n4:{date4.IsWeekPassed()}");
+            Console.ReadLine();
+        }
+    }
+}
